dcac6497878d2b079b6c7e1477c4e298732ecac8
Brandon L. Borrero <borrero509@outlook.com>
2019-06-21 10:50:20 -0700

    MVP Done

--- names/names.py
+++ names/names.py
@@ -2,21 +2,70 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open("names_1.txt", "r")
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open("names_2.txt", "r")
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# OG Solution
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        elif value > self.value:
+            if not self.right:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left:
+                if self.left.value == target:
+                    return True
+                else:
+                    return self.left.contains(target)
+        elif target >= self.value:
+            if self.right:
+                if self.right.value == target:
+                    return True
+                else:
+                    return self.right.contains(target)
+        else:
+            return False
+
+
+checkNames = BST("m")
+
+for name1 in names_1:
+    checkNames.insert(name1)
+
+for name2 in names_2:
+    if checkNames.contains(name2):
+        duplicates.append(name2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
